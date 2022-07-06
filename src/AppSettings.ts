8cb45c3bd6812b7c4b734f04a67b2dc795ffea86
V. James Krammes <krammes@comcast.net>
2022-07-06 15:26:31 -0400

    Change to production server uri

--- src/AppSettings.ts
+++ src/AppSettings.ts
@@ -1,4 +1,5 @@
-export const server = 'https://localhost:5011/';
+//export const server = 'https://localhost:5011/';
+export const server = 'https://beansapi.azurewebsites.net/';
 
 export const ApiBase = `${server}api/v1`;
 
